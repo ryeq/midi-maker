f05efb567dfb7225f4cd2b2fc5ad3faa0c81ffa9
Quiros Ryan <ryan.quiros@edu-poly.com>
2025-05-09 00:57:33 +0000

    Replace the logo on the UI with the attached file.

--- src/components/canb-logo.tsx
+++ src/components/canb-logo.tsx
@@ -6,7 +6,7 @@ export function CanbLogo(props: SVGProps<SVGSVGElement>) {
 
   return (
     <svg
-      viewBox="0 0 330 80" // Width: C(80)+space(10)+A(70)+space(10)+N(80)+space(10)+B(70)=330
+      viewBox="0 0 300 80" // Width: C(80)+A(70)+N(80)+B(70)=300 (spacing=0)
       xmlns="http://www.w3.org/2000/svg"
       aria-label="CANB Logo"
       {...props}
@@ -22,20 +22,20 @@ export function CanbLogo(props: SVGProps<SVGSVGElement>) {
 
       {/* A shape: Triangle */}
       {/* In an 70x80 box. Base from (0,80) to (70,80), peak at (35,0) relative to its group */}
-      <g transform="translate(90,0)"> {/* 80 (C width) + 10 (space) */}
+      <g transform="translate(80,0)"> {/* C_width(80) */}
         <path d="M 0,80 L 70,80 L 35,0 Z" /> {/* Full height triangle */}
       </g>
 
       {/* N shape: Semi-ellipse on top, V-shape for bottom. rx=40, ry=40. V-point at (40,80) */}
       {/* In an 80x80 box. */}
-      <g transform="translate(170,0)"> {/* 90 (C_offset) + 70 (A_width) + 10 (space) */}
+      <g transform="translate(150,0)"> {/* C_width(80) + A_width(70) */}
         <path d="M 0,40 A 40,40 0 0 1 80,40 L 40,80 Z" /> {/* V-point at y=80 */}
       </g>
 
       {/* B shape: Spine on left, two rightward curves. */}
       {/* Spine width 20. Bottom lobe rx=50,ry=20. Top lobe rx=45,ry=20. Total height 80. Each lobe height 40. */}
       {/* Total width of B approx 70. */}
-      <g transform="translate(260,0)"> {/* 170 (N_offset) + 80 (N_width) + 10 (space) */}
+      <g transform="translate(230,0)"> {/* C_width(80) + A_width(70) + N_width(80) */}
         {/* Corrected sweep-flag from 0 to 1 for rightward curves */}
         <path d="M0,0 V80 H20 A50,20 0 0 1 20,40 A45,20 0 0 1 20,0 H0 Z" />
       </g>
